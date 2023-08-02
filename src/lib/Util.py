784073975d57dd7bf7176ca40d466f1a3523dda5
Matthew Smith <matthew.smith@sol1.com.au>
2023-08-02 11:05:17 +1000

    add processid to logging

--- src/lib/Util.py
+++ src/lib/Util.py
@@ -1,9 +1,9 @@
 from loguru import logger
 
 def initLogger(log_disable_file = False, log_level = "INFO", log_file = "/var/log/icinga2/notification_script.log", rotate = '1 day', retention = '2 days'):
-    format = "<blue>{time:YYYY-MM-DD HH:mm:ss.SSS}</blue> <level>{level}</level>: {message}"
+    format = "<blue>{time:YYYY-MM-DD HH:mm:ss.SSS}</blue> <yellow>({process.id})</yellow> <level>{level}</level>: {message}"
     if log_level == "DEBUG" and not log_disable_file:
-        format = "<blue>{time:YYYY-MM-DD HH:mm:ss.SSS}</blue> <cyan>{function}</cyan>:<cyan>{line}</cyan> <level>{level}</level>: {message}"
+        format = "<blue>{time:YYYY-MM-DD HH:mm:ss.SSS}</blue> <yellow>({process.id})</yellow> <cyan>{function}</cyan>:<cyan>{line}</cyan> <level>{level}</level>: {message}"
     else:
         logger.remove()
     if not log_disable_file:

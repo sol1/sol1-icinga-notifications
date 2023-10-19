cfb3d45a7eed8a66d4a40f7247d8fb86b9284419
63 <55000067+63@users.noreply.github.com>
2023-10-19 12:06:09 +1100

    Read netbox object and impact assessment

--- src/netbox-path-impact-notification.py
+++ src/netbox-path-impact-notification.py
@@ -12,6 +12,7 @@ from lib.SettingsParser import SettingsParser
 from lib.Util import initLogger
 
 from loguru import logger
+import pynetbox
 
 # Helper to load config from file
 @dataclasses.dataclass
@@ -25,6 +26,9 @@ class SettingsNetbox(SettingsFile):
     # INFO: no trailing / on the url. eg: http://netbox.domain.local
     url: str = ''
     token: str = 'abcdefghijklmnopqrstuvwxyz1234567890'
+    api_device: str = ''
+    api_vm: str = ''
+    api_impact: str = ''
     proxy: str = ''
     timeout: str = 20
     _json_dict_key: str = 'netbox'
@@ -99,10 +103,99 @@ class Settings(SettingsParser):
 
 
 class Netbox:
-    def __init__(self) -> None:
-        self.headers = {
-            'Content-Type': 'application/json'
+    """Netbox object that parses data from the Netbox api
+    all ivars are intialized as empty and filled if valid data is found for each type
+
+    :ivar host: dict : api data to the host for the NETBOXBASE and host_name
+    :ivar host_url: str : url to the host for the NETBOXBASE and host_name
+    :ivar ip: dict : api data to the host ip address for the NETBOXBASE and host_name
+    :ivar ip_url: str : url to the host ip address for the NETBOXBASE and host_name
+    """
+
+    def __init__(self, config):
+        self.config = config
+        self.host = {}
+        self.host_url = ''
+        self.ip_url = ''
+        self.type = ''
+        logger.debug(f"Initalizing the Netbox class with host: {self.config.host_name}, netbox url: {self.config.netbox.url}")
+
+        if self.config.netbox:
+            self.__parse()
+
+    def __parse(self):
+        """ Search netbox for
+
+        :return:
+        """
+        nb_device = self.__searchData(self.config.netbox.url + self.config.netbox.api_device + '/?name=' + self.config.host_name)
+        nb_vm = self.__searchData(self.config.netbox.url + self.config.netbox.api_vm + '/?name=' + self.config.host_name)
+
+        logger.debug(json.dumps(nb_device, indent=4, sort_keys=True))
+        logger.debug(json.dumps(dict(nb_vm), indent=4, sort_keys=True))
+        if nb_device and not nb_vm:
+            self.host = nb_device
+            self.type = self.config.netbox.api_device.split('/api')[1].replace('/', '.')
+            self.host_url = "{}/{}/".format(self.config.netbox.url + self.config.netbox.api_device, nb_device['id'])
+        elif not nb_device and nb_vm:
+            self.host = nb_vm
+            self.type = self.config.netbox.api_vm.split('/api/')[1].replace('/', '.')
+            self.host_url = "{}/{}/".format(self.config.netbox.url + self.config.netbox.api_vm, nb_vm['id'])
+        elif nb_device and nb_vm:
+            logger.info("Found multiple device's or vm's that match")
+        else:
+            logger.warning("Found no device's or vm's that match")
+
+    def getImpactAssessment(self):
+        args = {
+            'url': f'{self.config.netbox.url}{self.config.netbox.api_impact}',
+            'params': { 'id': self.host['id'], 'type': self.type },
+            'timeout': self.config.netbox.timeout,
+            'headers': {'Accept': 'application/json'}
         }
+        if self.config.netbox.proxy:
+            args['proxies'] = {'http': self.config.netbox.proxy, 'https': self.config.netbox.proxy}
+
+        if self.config.netbox.token:
+            args['headers'].update({'Authorization': 'Token ' + self.config.netbox.token})
+
+        try:
+            logger.debug(f"Netbox request to url: {args['url']}")
+            response = requests.get(**args)
+            result = response.json()
+        except Exception as e:
+            logger.error("Error getting netbox data from {} with error {}".format(args['url'], e))
+            result = {'count': 0}
+        logger.debug(f"Netbox result: {result}")
+        return result
+
+    def __getServerData(self, url):
+        args = {
+            'url': url,
+            'timeout': self.config.netbox.timeout,
+            'headers': {'Accept': 'application/json'}
+        }
+        if self.config.netbox.proxy:
+            args['proxies'] = {'http': self.config.netbox.proxy, 'https': self.config.netbox.proxy}
+
+        if self.config.netbox.token:
+            args['headers'].update({'Authorization': 'Token ' + self.config.netbox.token})
+        try:
+            logger.debug(f"Netbox request to url: {url}")
+            response = requests.get(**args)
+            result = response.json()
+        except Exception as e:
+            logger.error("Error getting netbox data from {} with error {}".format(url, e))
+            result = {'count': 0}
+        logger.debug(f"Netbox result: {result}")
+        return result
+
+    def __searchData(self, url):
+        result = self.__getServerData(url)
+        if result['count'] == 1 and result['results']:
+            return result['results'][0]
+        else:
+            return {}
 
 class SMS:
     def __init__(self, token, url) -> None:
@@ -133,7 +226,20 @@ if __name__ == "__main__":
 
     logger.debug(json.dumps(dataclasses.asdict(config), indent=2))
 
-    netbox = Netbox()
-    sms = SMS()
-    sms.bobSMS()
+    netbox = Netbox(config)
+    impacted_paths = netbox.getImpactAssessment()
+
+    for path in impacted_paths:
+        logger.info(f"Path: {path['name']}")
+        for contact in path['contacts']:
+            logger.info(f"Contact: {contact['name']}")
+            logger.info(f"Email: {contact['email']}")
+            logger.info(f"Phone: {contact['phone']}")
+        for object in path['objects']:
+            if len(object['direction']) == 0:
+                logger.info(f"Object: {object['name']} - {object['type']} - {object['description']}")
+        
+
+    #sms = SMS()
+    #sms.bobSMS()
     # Do stuff kinda, make the class do the work
